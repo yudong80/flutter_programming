43d8f54faaba533af46e5873871cd94f3b5dfd8e
yudong80 <koreacio@gmail.com>
2019-06-04 22:46:59 +0900

    Add control_flows dart

--- dart_lang/lib/control_flows.dart
+++ dart_lang/lib/control_flows.dart
@@ -1,15 +1,51 @@
 void main() {
   //1. if
+  int even = 78;
+  int odd = 99;
+  if (even % 2 == 0) {
+    print('$even is even number');
+  }
+
+  if (odd % 2 == 0) {
+    print('$odd is not even number');
+  } else {
+    print('$odd is odd number');
+  }
 
   //2. for
-
+  List<String> fruits = ['Apple', 'Banana', "Kiwi"];
+  for (String fruit in fruits) {
+    print('I like $fruit');
+  }
 
   //3. while
-
+  List<num> numbers = [100, 200, -1];
+  int i=0;
+  while(numbers[i] > 0) {
+    print('${numbers[i]} is positive');
+    i = i + 1;
+  }
 
   //4. do-while
+  int j = 5;
+  do {
+    print(j);
+    j--;
+  } while(j > 0);
 
-
-  //5. switch 
-
+  //5. switch / case
+  List<num> httpCodes = [200, 401, 500];
+  for (num http in httpCodes) {
+    switch(http) {
+      case 200:
+        print('200 is OK');
+        break;
+      case 401:
+        print('400 is Unautherized');
+        break;
+      case 500:
+        print('500 is Internal Server Error');
+        break;
+    }
+  }
 }
---
+++ dart_lang/lib/final_const_static.dart
@@ -0,0 +1,8 @@
+void main() {
+  //1. final
+
+  //2. const
+
+  //3. static
+
+}
