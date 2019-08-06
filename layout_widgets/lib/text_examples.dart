71e68eded5a1e293e27b7d3100e679249214a4a8
donghwan.yu <koreacio@gmail.com>
2019-08-06 21:15:13 +0900

    [text_widget] fix it

--- layout_widgets/lib/text_examples.dart
+++ layout_widgets/lib/text_examples.dart
@@ -3,9 +3,12 @@ import 'package:flutter/material.dart';
 void main() => runApp(TextDemo());
 
 class TextDemo extends StatelessWidget {
-  final String _title = 'Text Widget Demo';
-  final String _name = 'Tony';
-  final int _age = 28;
+  static const String _title = "Text 위젯 데모";
+  static const String _name = 'Tony Stark';
+  static const String _longText = """
+  플러터(Flutter)는 구글이 개발한 오픈 소스 모바일 애플리케이션 개발 프레임워크이다. 
+  안드로이드, iOS용 애플리케이션 개발을 위해, 또 구글 푸크시아용 애플리케이션 개발의 주된 방식으로 사용된다.
+  """;
 
   @override
   Widget build(BuildContext context) {
@@ -16,9 +19,9 @@ class TextDemo extends StatelessWidget {
           body: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: <Widget>[
-              Text("Simple Text"), //simple text FIXME 한글이 안되는데???
+              Text("단순 텍스트 표시"), //simple text
               Text(
-                'My name is $_name and $_age years old.', //styled text
+                'Styled Text with $_name', //styled text
                 style: TextStyle(
                     color: Colors.black,
                     fontSize: 20.0, //dp
@@ -28,8 +31,8 @@ class TextDemo extends StatelessWidget {
                     fontWeight: FontWeight.bold),
               ),
               Text(
-                'My hobby is coding Flutter. If you have any questions please free free to ask anything about it',
-                overflow: TextOverflow.ellipsis,
+                _longText,
+                overflow: TextOverflow.ellipsis, //text overflow
               ),
             ],
           ),
