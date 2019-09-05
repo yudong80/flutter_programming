d0ac22109a2a3974232a2fde3e5be9bdd64fbbbc
donghwan.yu <koreacio@gmail.com>
2019-09-05 22:04:49 +0900

    [net_async] update subway v3

--- network_async/lib/page_subway_info.dart
+++ network_async/lib/page_subway_info.dart
@@ -1,17 +1,76 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
+const String _urlPrefix = 'http://swopenapi.seoul.go.kr/api/subway/';
+const String _userKey = 'sample';
+const String _urlSuffix = '/json/realtimeStationArrival/0/5/';
 const String _defaultStation = '광화문';
+
+const int STATUS_OK = 200;
 
 class MainPage extends StatefulWidget {
   @override
   State createState() => MainPageState();
 }
 
-class MainPageState extends State<MainPage> {
-  var _stationController = TextEditingController(text: _defaultStation);
+class SubwayArrival {
+  int _rowNum;
+  String _subwayId;
+  String _trainLineNm;
+  String _subwayHeading;
+  String _arvlMsg2;
 
-  void onClick() {
-    print('>> input : ${_stationController.text}');
+  SubwayArrival(this._rowNum, this._subwayId, this._trainLineNm, this._subwayHeading, this._arvlMsg2);
+
+  int get rowNum => _rowNum;
+  String get subwayId => _subwayId;
+  String get trainLineNm => _trainLineNm;
+  String get subwayHeading => _subwayHeading;
+  String get arvlMsg2 => _arvlMsg2;
+}
+
+class MainPageState extends State<MainPage> {
+  TextEditingController _stationController = TextEditingController(text: _defaultStation);
+  String _text1 = 'hello';
+  String _text2 = 'hello';
+
+  void _onClick() async {
+    var response = await http.get(_buildUrl(_defaultStation));
+    String responseBody = response.body;
+    print('res >> $responseBody');
+
+    var json = jsonDecode(responseBody);
+    Map<String, dynamic> errorMessage = json['errorMessage'];
+
+    if (errorMessage['status'] != STATUS_OK) {
+      setState(() {
+        final String errMessage = errorMessage['message'];
+        _text1 = 'error';
+        _text2 = errMessage;
+      });
+      return;
+    }
+
+    List<dynamic> realtimeArrivalList = json['realtimeArrivalList'];
+    final int cnt = realtimeArrivalList.length;
+
+    List<SubwayArrival> list = List.generate(cnt,
+            (int i) {
+          Map<String, dynamic> item = realtimeArrivalList[i];
+          return SubwayArrival(
+            item['rowNum'],
+            item['subwayId'],
+            item['trainLineNm'],
+            item['subwayHeading'],
+            item['arvlMsg2'],);
+        });
+
+    SubwayArrival res = list[0];
+    setState(() {
+      _text1 = res.trainLineNm;
+      _text2 = res.arvlMsg2;
+    });
   }
 
   @override
@@ -43,7 +102,7 @@ class MainPageState extends State<MainPage> {
                 ),
                 RaisedButton(
                   child: Text('조회'),
-                  onPressed: onClick,
+                  onPressed: _onClick,
                 ),
               ],
             ),
@@ -64,8 +123,8 @@ class MainPageState extends State<MainPage> {
                     aspectRatio: 15.0 / 5.0,
                     child: Image.asset('images/subway.png'),
                   ),
-                  Text('hello1'),
-                  Text('hello2'),
+                  Text(_text1),
+                  Text(_text2),
                 ],
               ),
             ),
@@ -74,4 +133,13 @@ class MainPageState extends State<MainPage> {
       ),
     );
   }
+}
+
+String _buildUrl(String station) {
+  StringBuffer sb = StringBuffer();
+  sb.write(_urlPrefix);
+  sb.write(_userKey);
+  sb.write(_urlSuffix);
+  sb.write(station);
+  return sb.toString();
 }
