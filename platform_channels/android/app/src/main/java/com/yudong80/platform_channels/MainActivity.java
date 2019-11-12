e41b20955ff7d4a09ab77fa9af3bf28835d0f628
donghwan.yu <koreacio@gmail.com>
2019-11-12 22:46:03 +0900

    [channels] add page_battery.dart

--- platform_channels/android/app/src/main/java/com/yudong80/platform_channels/MainActivity.java
+++ platform_channels/android/app/src/main/java/com/yudong80/platform_channels/MainActivity.java
@@ -1,13 +1,32 @@
 package com.yudong80.platform_channels;
 
+import android.os.BatteryManager;
+
 import androidx.annotation.NonNull;
 import io.flutter.embedding.android.FlutterActivity;
 import io.flutter.embedding.engine.FlutterEngine;
+import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugins.GeneratedPluginRegistrant;
 
+import static android.os.BatteryManager.BATTERY_PROPERTY_CAPACITY;
+
 public class MainActivity extends FlutterActivity {
+  private static final String METHOD_BATTERY = "getBatteryLevel";
+  private static final String CHANNEL_BATTERY = "android/battery";
+
   @Override
   public void configureFlutterEngine(@NonNull FlutterEngine flutterEngine) {
     GeneratedPluginRegistrant.registerWith(flutterEngine);
+
+    new MethodChannel(flutterEngine.getDartExecutor(), CHANNEL_BATTERY).setMethodCallHandler(
+        (call, result) -> {
+          if(METHOD_BATTERY.equals(call.method)) {
+            BatteryManager manager = (BatteryManager) getSystemService(BATTERY_SERVICE);
+            int battery = manager.getIntProperty(BATTERY_PROPERTY_CAPACITY);
+            result.success(battery);
+          }
+          result.notImplemented();
+        }
+    );
   }
 }
--- platform_channels/lib/main.dart
+++ platform_channels/lib/main.dart
@@ -1,7 +1,4 @@
-import 'dart:async';
-
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 
 void main() => runApp(BatteryPage());
 
@@ -13,24 +10,8 @@ class BatteryPage extends StatefulWidget {
 class BatteryPageState extends State<BatteryPage> {
   String _text = 'battery level is unknown';
 
-  static const String CHANNEL_BATTERY = 'android/battery';
-  static const String METHOD_BATTERY = 'getBatteryLevel';
-  static const MethodChannel batteryChannel = MethodChannel(CHANNEL_BATTERY);
-
-  Future<void> _refresh() async {
-    print('refresh battery level');
-
-    String _newText;
-    try {
-      final int result = await batteryChannel.invokeMethod(METHOD_BATTERY);
-      _newText = 'battery level is $result %';
-    } on PlatformException {
-      _newText = 'battery level is unavailable';
-    }
-
-    setState(() {
-      _text = 'battery level is $_newText';
-    });
+  void _refresh() {
+    print('refresh');
   }
 
   @override
