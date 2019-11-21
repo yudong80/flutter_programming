8fe7e2fca12d8bc0d7911cef3915b70d890990c1
donghwan.yu <koreacio@gmail.com>
2019-11-21 23:30:46 +0900

    [hello] fix project

---
+++ hello_flutter/lib/simple_app.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+void main() =>
+    runApp(MaterialApp(
+        title: 'Hello Flutter',
+        home: Scaffold(
+          appBar: AppBar(title: Text('Hello Flutter')),
+          body: Text('Hello Flutter'),
+        ),
+    ));
