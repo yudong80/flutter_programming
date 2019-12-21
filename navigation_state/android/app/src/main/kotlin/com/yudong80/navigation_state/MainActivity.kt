469679f189536e2240c8953fc1fca59c750a5e6a
donghwan.yu <koreacio@gmail.com>
2019-12-21 14:44:59 +0900

    [navi] fix ios lib

---
+++ navigation_state/android/app/src/main/kotlin/com/yudong80/navigation_state/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.yudong80.navigation_state
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
